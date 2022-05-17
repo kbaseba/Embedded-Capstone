3f7a865e9b39a7d6a342e690b3d19c4b4800719b
Cameron <2propylheptane@gmail.com>
2022-05-17 14:51:39 -0700

    Added sound demo

--- pi code/threadfunctions.c
+++ pi code/threadfunctions.c
@@ -10,34 +10,35 @@
 //imageRec:
 //Authers:
 void *imageRec(void* mem_ptr) {
-   printf("Image Recognition works \n");
+   //printf("Image Recognition works \n");
    pthread_exit(NULL);
 }
 
 //reading:
 //Authers:
 void *reading(void* mem_ptr) {
-   printf("Reading works \n");
+   //printf("Reading works \n");
    pthread_exit(NULL);
 }
 
 //toString:
 //Authers:
 void *toString(void* mem_ptr) {
-   printf("toString works \n");
+   //printf("toString works \n");
    pthread_exit(NULL);
 }
 
 //concatenation:
 //Authers:
 void *concatenation(void* mem_ptr) {
-   printf("Concatenation works \n");
+   //printf("Concatenation works \n");
    pthread_exit(NULL);
 }
 
 //audioOut:
 //Authers:
 void *audioOut(void* mem_ptr) {
-   printf("Audio Output works \n");
+   system("aplay /usr/share/sounds/alsa/Front_Center.wav");
+	printf("Audio Output works \n");
    pthread_exit(NULL);
 }
