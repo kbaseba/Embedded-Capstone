6aa5c7b0d5146fe1bc25c836ffa6e6e178f98072
slienhmjgvgerghrgewahrfvvnmjeige <2propylheptane@gmail.com>
2022-05-19 17:55:22 -0700

    suppress messages when playing audio

--- pi code/threadfunctions.c
+++ pi code/threadfunctions.c
@@ -28,7 +28,8 @@ void *reading(void* mem_ptr) {
     int i = 0; // Run through some distances to test sound
     while(i <25){
        mem_ptr_->stm_data.distance = (double)i;
-       delay(10000);
+       delay(1000);
+       i++;
     }
 
     printf("Reading Finished \n");
@@ -59,7 +60,7 @@ void *audioOut(void* mem_ptr) {
     int i = 0; // Used to auto stop execution
     while(i <250){
        if(mem_ptr_->controls_data.distance_on){
-            system("aplay beep-01a.wav");
+            system("aplay -q beep-01a.wav");
             delay(100 * mem_ptr_->stm_data.distance);
        } else if(mem_ptr_->controls_data.recognition_on){
             // Play sound recognition audio
