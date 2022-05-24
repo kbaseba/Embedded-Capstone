f56007370e456f4bef4f1472e03042fe6039ae24
slienhmjgvgerghrgewahrfvvnmjeige <2propylheptane@gmail.com>
2022-05-24 16:24:17 -0700

    Updated beep path

--- src/pi_code/threadfunctions.c
+++ src/pi_code/threadfunctions.c
@@ -60,7 +60,7 @@ void *audioOut(void* mem_ptr) {
     int i = 0; // Used to auto stop execution
     while(i <250){
        if(mem_ptr_->controls_data.distance_on){
-            system("aplay -q beep-01a.wav");
+            system("aplay -q /usr/local/bin/horus-res/beep-01a.wav");
             delay(100 * mem_ptr_->stm_data.distance);
        } else if(mem_ptr_->controls_data.recognition_on){
             // Play sound recognition audio
