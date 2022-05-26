6e1f6247663d3a4ac99be240b1214d1e04079967
slienhmjgvgerghrgewahrfvvnmjeige <2propylheptane@gmail.com>
2022-05-26 16:12:47 -0700

    Enabled object detection

--- src/pi_code/threadfunctions.c
+++ src/pi_code/threadfunctions.c
@@ -14,7 +14,7 @@
 void *objectRec(void* mem_ptr) {
     //MemoryStructure *mem_ptr_ = mem_ptr;
     // Start recognition script
-    //system("python /usr/local/lib/horus-res/project/Object_Detection_Picamera.py");
+    system(". /usr/local/lib/horus-res/project/env/bin/activate && python /usr/local/lib/horus-res/project/Object_Detection_Picamera.py");
     //while
         //if control is off
             //end/halt process
